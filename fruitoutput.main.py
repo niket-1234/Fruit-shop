e2bb6f6b2e6249269e28a6af7abd3b0cf9deeb38
niket-1234 <157384170+niket-1234@users.noreply.github.com>
2024-01-22 21:03:23 +0530

    Add files via upload

---
+++ fruitoutput.main.py
@@ -0,0 +1,129 @@
+WELCOME TO GARDEN GROVE BAZAAR
+
+
+CRAVINGS FOR SOME AMAZING FRUITS?
+THEN YOU'RE AT THE RIGHT SPOT!
+
+
+CHOOSE AN OPTION TO..
+1. Login
+2. Signup
+3. Exit
+Enter your choice: 
+2
+Enter a new username: 
+dave
+Enter a password: 
+qwerty
+Account created successfully!
+Welcome, dave!
+choose an option to
+
+
+1. Display Fruit Menu
+2. Add to Cart
+3. Display Cart
+4. Checkout
+5. Exit
+Enter your choice: 
+1
+Fruit Menu:
+1. Apple - $2.5 | Stock: 10
+2. Banana - $1.5 | Stock: 15
+3. Orange - $3.0 | Stock: 8
+4. Mango - $4.0 | Stock: 500
+5. Watermelon - $2.0 | Stock: 50
+6. Kiwi - $6.0 | Stock: 100
+7. Guava - $5.0 | Stock: 300
+8. Papaya - $4.0 | Stock: 200
+9. Grape - $5.0 | Stock: 350
+10. Cherry - $1.0 | Stock: 250
+choose an option to
+
+
+1. Display Fruit Menu
+2. Add to Cart
+3. Display Cart
+4. Checkout
+5. Exit
+Enter your choice: 
+2
+Fruit Menu:
+1. Apple - $2.5 | Stock: 10
+2. Banana - $1.5 | Stock: 15
+3. Orange - $3.0 | Stock: 8
+4. Mango - $4.0 | Stock: 500
+5. Watermelon - $2.0 | Stock: 50
+6. Kiwi - $6.0 | Stock: 100
+7. Guava - $5.0 | Stock: 300
+8. Papaya - $4.0 | Stock: 200
+9. Grape - $5.0 | Stock: 350
+10. Cherry - $1.0 | Stock: 250
+Enter the number of the fruit to add to cart: 
+6
+Enter the quantity of Kiwi: 
+38
+38 Kiwi(s) added to your cart.
+choose an option to
+
+
+1. Display Fruit Menu
+2. Add to Cart
+3. Display Cart
+4. Checkout
+5. Exit
+Enter your choice: 
+2
+Fruit Menu:
+1. Apple - $2.5 | Stock: 10
+2. Banana - $1.5 | Stock: 15
+3. Orange - $3.0 | Stock: 8
+4. Mango - $4.0 | Stock: 500
+5. Watermelon - $2.0 | Stock: 50
+6. Kiwi - $6.0 | Stock: 62
+7. Guava - $5.0 | Stock: 300
+8. Papaya - $4.0 | Stock: 200
+9. Grape - $5.0 | Stock: 350
+10. Cherry - $1.0 | Stock: 250
+Enter the number of the fruit to add to cart: 
+8
+Enter the quantity of Papaya: 
+25
+25 Papaya(s) added to your cart.
+choose an option to
+
+
+1. Display Fruit Menu
+2. Add to Cart
+3. Display Cart
+4. Checkout
+5. Exit
+Enter your choice: 
+3
+
+Your Cart:
+1. Kiwi - Quantity: 38
+2. Papaya - Quantity: 25
+choose an option to
+
+
+1. Display Fruit Menu
+2. Add to Cart
+3. Display Cart
+4. Checkout
+5. Exit
+Enter your choice: 
+4
+_____________________________________
+INVOICE
+
+Your total bill is: $328.0 
+
+Thank You for your purchase 
+
+PLEASE DO VISIT US AGAIN
+_____________________________________
+
+
+** Process exited - Return Code: 0 **
+Press Enter to exit terminal
